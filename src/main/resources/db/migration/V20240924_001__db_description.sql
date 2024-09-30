1273f048f4678ab1d02c38174f3f049b5e80fc15
ian-readman <ian.readman@hmcts.net>
2024-09-30 15:26:42 +0100

    PO-793_2 DB, Flyway and Terraform Config

--- src/main/resources/db/migration/V20240924_001__db_description.sql
+++ src/main/resources/db/migration/V20240924_001__db_description.sql
@@ -1,4 +1,5 @@
 /**
+*
 * CGI OPAL Program
 *
 * MODULE      : database_comment.sql
@@ -12,6 +13,5 @@
 * 24/09/2024    I Readman   1.0         PO-793 Create File Handler Database
 *
 **/
-
 -- Database build required an inital migration file so adding a description to the database
 COMMENT ON DATABASE "opal-file-db" IS 'Opal File Handler Database';
