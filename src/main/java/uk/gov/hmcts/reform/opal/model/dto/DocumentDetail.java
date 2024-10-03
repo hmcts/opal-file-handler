05c0de6080d9fa07d60d7b2736d8afeb3af4e82e
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-10-03 11:01:19 +0100

    Streamline util and update tests

--- src/main/java/uk/gov/hmcts/reform/opal/model/dto/DocumentDetail.java
+++ src/main/java/uk/gov/hmcts/reform/opal/model/dto/DocumentDetail.java
@@ -1,15 +1,27 @@
 package uk.gov.hmcts.reform.opal.model.dto;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
 import lombok.Data;
 
 @Data
+@XmlAccessorType(XmlAccessType.FIELD)
 public  class DocumentDetail {
+    @XmlElement(name = "CustomerRef", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String customerRef;
+    @XmlElement(name = "RecordType", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String recordType;
+    @XmlElement(name = "LocationCode", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String locationCode;
+    @XmlElement(name = "NationalInsuranceNumber", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String nationalInsuranceNumberType;
+    @XmlElement(name = "DateFrom", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String dateFrom;
+    @XmlElement(name = "DateTo", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String dateTo;
+    @XmlElement(name = "DetailAmount", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String detailAmountType;
+    @XmlElement(name = "DetailAmountSign", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String detailAmountSign;
 }
--- src/main/java/uk/gov/hmcts/reform/opal/util/XMLUtil.java
+++ src/main/java/uk/gov/hmcts/reform/opal/util/XMLUtil.java
@@ -5,7 +5,6 @@ import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 
-import java.io.File;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -38,31 +37,6 @@ public class XMLUtil {
     }
 
     /**
-     * Marshals (converts) a Java object to an XML file.
-     *
-     * @param object the object to marshal
-     * @param file the file to write the XML to
-     * @throws JAXBException if any error occurs during the marshalling process
-     */
-    public static void marshalToFile(Object object, File file) throws JAXBException {
-        if (object == null) {
-            throw new IllegalArgumentException("Object to marshal cannot be null.");
-        }
-        if (file == null) {
-            throw new IllegalArgumentException("File to write to cannot be null.");
-        }
-
-        // Create a JAXBContext for the object's class
-        JAXBContext context = JAXBContext.newInstance(object.getClass());
-        // Create a marshaller
-        Marshaller marshaller = context.createMarshaller();
-        // Optional: Set formatted output (pretty print)
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        // Marshal the object to a file
-        marshaller.marshal(object, file);
-    }
-
-    /**
      * Unmarshals (converts) an XML string to a Java object.
      *
      * @param <T> the type of the object to unmarshal to
@@ -85,28 +59,5 @@ public class XMLUtil {
         Unmarshaller unmarshaller = context.createUnmarshaller();
         // Unmarshal the XML to a Java object
         return (T) unmarshaller.unmarshal(reader);
-    }
-
-    /**
-     * Unmarshals (converts) an XML file to a Java object.
-     *
-     * @param <T> the type of the object to unmarshal to
-     * @param file the XML file to unmarshal
-     * @param clazz the class of the object to unmarshal
-     * @return the unmarshalled object
-     * @throws JAXBException if any error occurs during the unmarshalling process
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T unmarshalFromFile(File file, Class<T> clazz) throws JAXBException {
-        if (file == null || !file.exists()) {
-            throw new IllegalArgumentException("File to unmarshal cannot be null or does not exist.");
-        }
-
-        // Create a JAXBContext for the class
-        JAXBContext context = JAXBContext.newInstance(clazz);
-        // Create an unmarshaller
-        Unmarshaller unmarshaller = context.createUnmarshaller();
-        // Unmarshal the file to a Java object
-        return (T) unmarshaller.unmarshal(file);
     }
 }
