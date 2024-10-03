eaf62f7dd61f615d45eba3a7cea3052f423e6201
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-10-03 09:42:40 +0100

    integration properties

--- src/integrationTest/java/uk/gov/hmcts/reform/opal/BaseIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/opal/BaseIntegrationTest.java
@@ -1,17 +1,28 @@
 package uk.gov.hmcts.reform.opal;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import uk.gov.hmcts.reform.opal.scheduler.config.CronJobConfiguration;
+import uk.gov.hmcts.reform.opal.scheduler.config.QuartzConfiguration;
+import uk.gov.hmcts.reform.opal.service.JobService;
 
 @Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles({"integration"})
 @SuppressWarnings("HideUtilityClassConstructor")
 public class BaseIntegrationTest {
+
+    @MockBean
+    CronJobConfiguration cronJobConfiguration;
+    @MockBean
+    QuartzConfiguration quartzConfiguration;
+    @MockBean
+    JobService jobService;
 
     @ServiceConnection
     @Container
