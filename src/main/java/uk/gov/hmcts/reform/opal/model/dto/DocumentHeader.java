81ff0459fde4b272364f5a37e6d1ff1df67ba297
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-10-03 11:01:19 +0100

    Streamline util and update tests

--- src/main/java/uk/gov/hmcts/reform/opal/model/dto/DocumentHeader.java
+++ src/main/java/uk/gov/hmcts/reform/opal/model/dto/DocumentHeader.java
@@ -1,15 +1,21 @@
 package uk.gov.hmcts.reform.opal.model.dto;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
+@XmlAccessorType(XmlAccessType.FIELD)
 public  class DocumentHeader {
+    @XmlElement(name = "CreditorID", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String creditorID;
+    @XmlElement(name = "BatchNumber", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String batchNumber;
-    private LocalDateTime pacsDocumentCreationDate;
+    @XmlElement(name = "PacsDocumentCreationDate", namespace = "http://www.dwp.gsi.gov.uk/pacs")
+    private String pacsDocumentCreationDate;
+    @XmlElement(name = "PacsDocumentCreationTime", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String pacsDocumentCreationTime;
+    @XmlElement(name = "NotificationReference", namespace = "http://www.dwp.gsi.gov.uk/pacs")
     private String notificationReference;
-
 }
